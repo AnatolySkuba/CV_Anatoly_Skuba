3776bce623c5d49893a6e51e20b211ccb5b6c1ac
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-08-10 04:47:20 +0300

    save-my-progress

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -2,19 +2,19 @@ import {
 	Container,
 	Name,
 	Profession,
+	DivFlex,
+	ImgMobile,
 	Img,
 	Title,
-	List,
-	Item,
-	TechSkill,
-	Link,
+	AddressItem,
+	TechItem,
 	Icon,
 	Span,
 	Section,
 	Paragraph,
-	LinkProject,
-	SubTitle,
-	WorkList,
+	LinkFirst,
+	Link,
+	ParagraphSelected,
 	WorkItem,
 } from "./App.styled";
 import photo from "assets/myPhoto.JPG";
@@ -23,140 +23,148 @@ function App() {
 	return (
 		<>
 			<Container>
-				<Name>ANATOLII SKUBA</Name>
-				<Profession>FRONT-END DEVELOPER</Profession>
-				<Img src={photo} alt="my photo" />
-				<Title>CONTACT INFORMATION</Title>
-				<address>
-					<List>
-						<Item>
-							<Link href="tel:+380661749466">+380661749466</Link>
-						</Item>
-						<Item>
-							<Link href="mailto:skuba.anatolii@gmail.com">skuba.anatolii@gmail.com</Link>
-						</Item>
-						<Item>
-							<Link href="https://www.linkedin.com/in/skuba/">https://www.linkedin.com/in/skuba/</Link>
-						</Item>
-						<Item>
-							<Link href="https://github.com/AnatoliiSkuba">https://github.com/AnatoliiSkuba</Link>
-						</Item>
-						<Item>
-							<Link href="https://goo.gl/maps/DPupraGfuRYHBRc19">Kyiv, Ukraine</Link>
-						</Item>
-					</List>
-				</address>
+				<DivFlex>
+					<Img src={photo} alt="my photo" />
+					<div>
+						<Name>ANATOLII SKUBA</Name>
+						<Profession>FRONT-END DEVELOPER</Profession>
+
+						<ImgMobile src={photo} alt="my photo" />
+
+						<Title>CONTACT INFORMATION</Title>
+						<address>
+							<ul>
+								<AddressItem>
+									<a href="tel:+380661749466">+380661749466</a>
+								</AddressItem>
+								<AddressItem>
+									<a href="mailto:skuba.anatolii@gmail.com">skuba.anatolii@gmail.com</a>
+								</AddressItem>
+								<AddressItem>
+									<a href="https://www.linkedin.com/in/skuba/">https://www.linkedin.com/in/skuba/</a>
+								</AddressItem>
+								<AddressItem>
+									<a href="https://github.com/AnatoliiSkuba">https://github.com/AnatoliiSkuba</a>
+								</AddressItem>
+								<AddressItem>
+									<a href="https://goo.gl/maps/DPupraGfuRYHBRc19">Kyiv, Ukraine</a>
+								</AddressItem>
+							</ul>
+						</address>
+					</div>
+				</DivFlex>
 				<Title>TECH SKILLS</Title>
-				<List>
-					<TechSkill>
+				<ul>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/react.png"
 							alt="React"
 						/>
 						<Span>React</Span>
-					</TechSkill>
-					<TechSkill>
+					</TechItem>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/typescript.png"
 							alt="TypeScript"
 						/>
 						<Span>TypeScript</Span>
-					</TechSkill>
+					</TechItem>
 
-					<TechSkill>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/javascript.png"
 							alt="JavaScript"
 						/>
 						<Span>JavaScript</Span>
-					</TechSkill>
-					<TechSkill>
+					</TechItem>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/phaser-3.png"
 							alt="Phaser-3"
 						/>
 						<Span>Phaser-3</Span>
-					</TechSkill>
-					<TechSkill>
+					</TechItem>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/redux.png"
 							alt="Redux"
 						/>
 						<Span>Redux</Span>
-					</TechSkill>
-					<TechSkill>
+					</TechItem>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/apollo-client.png"
 							alt="Apollo Client"
 						/>
 						<Span>Apollo Client</Span>
-					</TechSkill>
-					<TechSkill>
+					</TechItem>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/graphql.png"
 							alt="GraphQL"
 						/>
 						<Span>GraphQL</Span>
-					</TechSkill>
-					<TechSkill>
+					</TechItem>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/sass.png"
 							alt="Sass"
 						/>
 						<Span>Sass</Span>
-					</TechSkill>
-					<TechSkill>
+					</TechItem>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/css-3.png"
 							alt="CSS-3"
 						/>
 						<Span>CSS-3</Span>
-					</TechSkill>
-					<TechSkill>
+					</TechItem>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/html-5.png"
 							alt="HTML-5"
 						/>
 						<Span>HTML-5</Span>
-					</TechSkill>
-					<TechSkill>
+					</TechItem>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/figma.png"
 							alt="Figma"
 						/>
 						<Span>Figma</Span>
-					</TechSkill>
-					<TechSkill>
+					</TechItem>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/photoshop.png"
 							alt="Photoshop"
 						/>
 						<Span>Photoshop</Span>
-					</TechSkill>
-					<TechSkill>
+					</TechItem>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/git.png"
 							alt="Git"
 						/>
 						<Span>Git</Span>
-					</TechSkill>
-					<TechSkill>
+					</TechItem>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/github.png"
 							alt="GitHub"
 						/>
 						<Span>GitHub</Span>
-					</TechSkill>
-					<TechSkill>
+					</TechItem>
+					<TechItem>
 						<Icon
 							src="https://raw.githubusercontent.com/AnatoliiSkuba/CV_Anatolii_Skuba/main/src/assets/webpack.png"
 							alt="Webpack"
 						/>
 						<Span>Webpack</Span>
-					</TechSkill>
-				</List>
+					</TechItem>
+				</ul>
 				<Title>SOFT SKILLS</Title>
-				<List>
+				<Paragraph>Punctual, Reliable, Teamwork, Flexible, Research, Creativity, Work ethic.</Paragraph>
+				{/* <ul>
 					<Item>Punctual,</Item>
 					<Item>Reliable,</Item>
 					<Item>Teamwork,</Item>
@@ -164,13 +172,14 @@ function App() {
 					<Item>Research,</Item>
 					<Item>Creativity,</Item>
 					<Item>Work ethic</Item>
-				</List>
+				</ul> */}
 				<Title>Languages</Title>
-				<List>
+				<Paragraph>English - Intermediate, Ukrainian - Native, Russian - Native</Paragraph>
+				{/* <ul>
 					<Item>English - Intermediate,</Item>
 					<Item>Ukrainian - Native,</Item>
 					<Item>Russian - Native</Item>
-				</List>
+				</ul> */}
 			</Container>
 			<Title>Summary</Title>
 			<Section>
@@ -206,43 +215,43 @@ function App() {
 				<Title>PROJECT EXPERIENCE</Title>
 				<ul>
 					<li>
-						<LinkProject href="https://anatoliiskuba.github.io/Scandiweb/">Scandiweb</LinkProject>,&nbsp;
-						<LinkProject href="https://github.com/AnatoliiSkuba/Scandiweb">Link</LinkProject> (React, JavaScript, Redux,
-						React Router Dom v6, Apollo Client, GraphQL). E-shop.
+						<LinkFirst href="https://anatoliiskuba.github.io/Scandiweb/">Scandiweb</LinkFirst>,&nbsp;
+						<Link href="https://github.com/AnatoliiSkuba/Scandiweb">Link</Link> (React, JavaScript, Redux, React Router
+						Dom v6, Apollo Client, GraphQL). E-shop.
 					</li>
 					<li>
-						<LinkProject href="https://anatoliiskuba.github.io/Phonebook/">Phonebook</LinkProject>,&nbsp;
-						<LinkProject href="https://github.com/AnatoliiSkuba/Phonebook">Link</LinkProject> (React, JavaScript, Redux,
-						RTK Query, React Router Dom v6, Material-UI). Application for storing phone numbers.
+						<LinkFirst href="https://anatoliiskuba.github.io/Phonebook/">Phonebook</LinkFirst>,&nbsp;
+						<Link href="https://github.com/AnatoliiSkuba/Phonebook">Link</Link> (React, JavaScript, Redux, RTK Query,
+						React Router Dom v6, Material-UI). Application for storing phone numbers.
 					</li>
 					<li>
-						<LinkProject href="https://anatoliiskuba.github.io/MyFantasy/">My Fantasy</LinkProject>,&nbsp;
-						<LinkProject href="https://github.com/AnatoliiSkuba/MyFantasy">Link</LinkProject> (TypeScript, Phaser 3,
-						Webpack). HTML5 game adapted for mobile devices.
+						<LinkFirst href="https://anatoliiskuba.github.io/MyFantasy/">My Fantasy</LinkFirst>,&nbsp;
+						<Link href="https://github.com/AnatoliiSkuba/MyFantasy">Link</Link> (TypeScript, Phaser 3, Webpack). HTML5
+						game adapted for mobile devices.
 					</li>
 					<li>
-						<LinkProject href="https://anatoliiskuba.github.io/Filmoteka/">Filmoteka</LinkProject>,&nbsp;
-						<LinkProject href="https://github.com/AnatoliiSkuba/Filmoteka">Link</LinkProject> (JavaScript, HTML, SASS).
-						Short-term team project. A web application that uses the Movie Database REST API.
+						<LinkFirst href="https://anatoliiskuba.github.io/Filmoteka/">Filmoteka</LinkFirst>,&nbsp;
+						<Link href="https://github.com/AnatoliiSkuba/Filmoteka">Link</Link> (JavaScript, HTML, SASS). Short-term
+						team project. A web application that uses the Movie Database REST API.
 					</li>
 				</ul>
 				<Title>WORK EXPERIENCE</Title>
-				<SubTitle>
+				<ParagraphSelected>
 					Founder of the network for the provision of photo services and the manufacture of duplicate keys.
 					Photomir.in.ua | 2009-2022
-					<WorkList>
-						<WorkItem>site development: HTML, CSS</WorkItem>
-						<WorkItem>photoshop design</WorkItem>
-						<WorkItem>interview and hiring of employees</WorkItem>
-					</WorkList>
-					Achievement: I created a self-employed business, created a website
-				</SubTitle>
+				</ParagraphSelected>
+				<ul>
+					<WorkItem>site development: HTML, CSS</WorkItem>
+					<WorkItem>photoshop design</WorkItem>
+					<WorkItem>interview and hiring of employees</WorkItem>
+				</ul>
+				<ParagraphSelected>Achievement: I created a self-employed business, created a website</ParagraphSelected>
 				<Title>EDUCATION</Title>
-				<SubTitle>National State Tax Service University of Ukraine</SubTitle>
+				<ParagraphSelected>National State Tax Service University of Ukraine</ParagraphSelected>
 				<Title>ADDITIONAL EDUCATION</Title>
-				<SubTitle>IT School GoIT (Kyiv) | 2021-2022</SubTitle>
+				<ParagraphSelected>IT School GoIT (Kyiv) | 2021-2022</ParagraphSelected>
 				<Paragraph>Full Stack Developer (React + Node)</Paragraph>
-				<SubTitle>American English Center | 2021-2022</SubTitle>
+				<ParagraphSelected>American English Center | 2021-2022</ParagraphSelected>
 			</Section>
 		</>
 	);
--- src/component/App/App.styled.js
+++ src/component/App/App.styled.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+	padding-left: 8px;
+	padding-right: 8px;
 	background-color: #e9f0f8;
 
 	@media screen and (min-width: 1300px) {
@@ -13,58 +15,73 @@ export const Container = styled.div`
 `;
 
 export const Name = styled.h1`
-	padding: 5vw;
+	padding: 18px;
 	text-align: center;
 	color: var(--accent-color);
 `;
 
 export const Profession = styled.p`
-	margin-bottom: 4.5vw;
+	margin-bottom: 16px;
+	font-size: 20px;
 	text-align: center;
 `;
 
-export const Img = styled.img`
+export const DivFlex = styled.div`
+	@media screen and (min-width: 556px) {
+		display: flex;
+	}
+`;
+
+export const ImgMobile = styled.img`
+	margin-left: -8px;
 	width: 100vw;
 	object-fit: contain;
+
+	@media screen and (min-width: 350px) {
+		display: flex;
+		width: 350px;
+		margin: 0 auto;
+	}
+
+	@media screen and (min-width: 556px) {
+		display: none;
+	}
+`;
+
+export const Img = styled.img`
+	display: none;
+
+	@media screen and (min-width: 556px) {
+		display: inline-block;
+		margin-left: -8px;
+		margin-right: 8px;
+		width: 45vw;
+		object-fit: contain;
+	}
 `;
 
 export const Title = styled.h2`
-	padding: 4.5vw 5.5vw 3vw;
+	padding: 16px;
 	text-align: center;
 	color: var(--accent-color);
 `;
 
-export const List = styled.ul`
-	padding: 0 5.5vw;
-`;
-
-export const Item = styled.li`
-	display: inline-block;
-	font-size: 5.5vw;
-
+export const AddressItem = styled.li`
 	&: not(: last-child) {
-		margin-right: 3vw;
-		margin-bottom: 2vw;
+		margin-bottom: 6px;
 	}
 `;
 
-export const Link = styled.a`
-	@media screen and (min-width: 1300px) {
-		font-size: 1.8vw;
-	}
-`;
-
-export const TechSkill = styled.li`
+export const TechItem = styled.li`
 	display: inline-block;
-	padding: 2vw;
-	font-size: 5vw;
+	padding: 8px;
 	background: #fff;
-	border-radius: 2vw;
+	border-radius: 8px;
 	box-shadow: 0 2px 2px rgb(220 230 248 / 50%);
 
 	&: not(: last-child) {
-		margin-right: 3vw;
-		margin-bottom: 3vw;
+		margin-right: 10px;
+		margin-bottom: 10px;
 	}
 `;
 
@@ -78,33 +95,34 @@ export const Span = styled.span`
 `;
 
 export const Paragraph = styled.p`
-	text-indent: 5vw;
-	font-size: 5vw;
+	text-indent: 20px;
 `;
 
 export const Section = styled.section`
-	padding: 0 5.5vw;
+	padding-left: 8px;
+	padding-right: 8px;
 `;
 
-export const LinkProject = styled.a`
-	font-size: 5.5vw;
+export const LinkFirst = styled.a`
+	margin-left: 20px;
+	font-weight: 600;
 	text-decoration: underline;
 	color: var(--accent-color);
+`;
+
+export const Link = styled.a`
 	font-weight: 600;
-	text-indent: 5vw;
-`;
-
-export const SubTitle = styled.p`
-	font-size: 5vw;
+	text-decoration: underline;
 	color: var(--accent-color);
-	font-weight: 500;
-	text-indent: 5vw;
 `;
 
-export const WorkList = styled.ul``;
+export const ParagraphSelected = styled.p`
+	color: var(--accent-color);
+	text-indent: 20px;
+`;
 
 export const WorkItem = styled.li`
-	margin-left: 5.5vw;
+	margin-left: 20px;
 	text-indent: 0;
 	list-style: disc;
 `;
